0ccfa02f6a150a4291da4c35d322e42c460f19d7
Kevin <kevin0696s@gmail.com>
2024-12-18 16:18:25 -0400

    Jira test implemented

---
+++ app/api/jira/download/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server"
+
+export async function POST(request:Request) {
+    const response = await request.json()
+    console.log(response)
+    
+    return NextResponse.json(
+        {
+            hola: "jkkakaka"
+        }
+    )
+}
