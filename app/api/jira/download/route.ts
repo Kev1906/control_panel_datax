a996eeaa3f5eb24b45c94836606eea4f46a613db
Kevin <kevin0696s@gmail.com>
2024-12-26 16:58:02 -0400

    changes

--- app/api/jira/download/route.ts
+++ app/api/jira/download/route.ts
@@ -2,8 +2,12 @@ import { NextResponse } from "next/server"
 
 export async function POST(request:Request) {
     const response = await request.json()
-    console.log(response['fields'])
-    console.log(response['fields']['customfield_10059'])
+    // console.log(response['fields'])
+    console.log(response['fields']['customfield_10056'])
+    console.log(response['fields']['customfield_10054'])
+    console.log(response['fields']['customfield_10061'])
+    console.log(response['fields']['customfield_10062'])
+    console.log(response['fields']['customfield_10063'])
     
     return NextResponse.json(
         {
