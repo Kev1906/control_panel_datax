62a425d12a2f7090d4e977674620b4762300d8af
Kevin <kevin0696s@gmail.com>
2024-12-26 16:31:33 -0400

    chanfes to see the request

--- app/api/jira/download/route.ts
+++ app/api/jira/download/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server"
 
 export async function POST(request:Request) {
     const response = await request.json()
+    console.log(response['fields'])
     console.log(response['fields']['customfield_10059'])
     
     return NextResponse.json(
